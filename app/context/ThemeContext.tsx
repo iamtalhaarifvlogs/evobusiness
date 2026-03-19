a9b31ce76459ce4270141aaee3b1f74950f32cad
iamtalhaarifvlogs <iamtalhaarifvlogs@gmail.com>
2026-03-19 15:32:46 +0500

    Update ThemeContext.tsx

--- app/context/ThemeContext.tsx
+++ app/context/ThemeContext.tsx
@@ -1,1 +1,35 @@
+"use client";
 
+import { createContext, useContext, useEffect, useState } from "react";
+
+type Theme = "light" | "dark";
+
+const ThemeContext = createContext<any>(null);
+
+export const ThemeProvider = ({ children }: any) => {
+  const [theme, setTheme] = useState<Theme>("light");
+
+  // Load saved theme
+  useEffect(() => {
+    const saved = localStorage.getItem("theme") as Theme;
+    if (saved) setTheme(saved);
+  }, []);
+
+  // Apply theme to body
+  useEffect(() => {
+    document.body.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prev: Theme) => (prev === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
