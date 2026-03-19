17612817dbfa56e9d6944efe3e8e32c2fbea8f79
iamtalhaarifvlogs <iamtalhaarifvlogs@gmail.com>
2026-03-19 18:09:41 +0500

    Update bootstrap-client.tsx

--- app/bootstrap-client.tsx
+++ app/bootstrap-client.tsx
@@ -4,7 +4,9 @@ import { useEffect } from "react";
 
 export default function BootstrapClient() {
   useEffect(() => {
-    import("bootstrap/dist/js/bootstrap.bundle.min.js");
+    if (typeof window !== "undefined") {
+      import("bootstrap/dist/js/bootstrap.bundle.min.js");
+    }
   }, []);
 
   return null;
