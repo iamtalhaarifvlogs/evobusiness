41ad5fb1dfef517eb31260c549239e0d66d0f119
iamtalhaarifvlogs <iamtalhaarifvlogs@gmail.com>
2026-03-19 18:04:37 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,10 @@
-
 import "@/app/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 import Providers from "@/app/components/providers";
 import Header from "@/app/components/Header";
 import Footer from "@/app/components/Footer";
-import { ThemeProvider } from "@/app/context/ThemeContext";
-
-export const metadata = {
-  title: "Evo Business",
-  description: "EvoDynamics Vision Business Platform",
-};
+import BootstrapClient from "@/app/bootstrap-client";
 
 export default function RootLayout({
   children,
@@ -21,19 +15,16 @@ export default function RootLayout({
     <html lang="en">
       <body className="app-body d-flex flex-column min-vh-100">
 
-        
-        {/* THEME PROVIDER WRAPS EVERYTHING */}
         <Providers>
-          
+          <BootstrapClient />
+
           <Header />
 
-          {/* MAIN CONTENT */}
           <main className="flex-grow-1 main-content">
             {children}
           </main>
 
           <Footer />
-
         </Providers>
 
       </body>
