ebbfe42855cddb630b1551a826f31989dee59d6f
iamtalhaarifvlogs <iamtalhaarifvlogs@gmail.com>
2026-03-20 06:16:08 +0500

    Update storage.ts

--- app/utils/storage.ts
+++ app/utils/storage.ts
@@ -1,1 +1,12 @@
+export const getStorage = (key: string) => {
+  if (typeof window === "undefined") return null;
 
+  const data = localStorage.getItem(key);
+  return data ? JSON.parse(data) : null;
+};
+
+export const setStorage = (key: string, value: any) => {
+  if (typeof window === "undefined") return;
+
+  localStorage.setItem(key, JSON.stringify(value));
+};
