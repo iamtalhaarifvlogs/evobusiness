600a8f80a3c2608a85eeb056abf185b89d7ffdc3
iamtalhaarifvlogs <iamtalhaarifvlogs@gmail.com>
2026-03-20 05:47:52 +0500

    Update route.ts

--- app/API/messages/route.ts
+++ app/API/messages/route.ts
@@ -1,1 +1,21 @@
+let messages: any[] = [];
 
+export async function GET() {
+  return Response.json(messages);
+}
+
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  const newMessage = {
+    id: Date.now(),
+    sender: body.sender,
+    text: body.text,
+    chatId: body.chatId,
+    createdAt: new Date(),
+  };
+
+  messages.push(newMessage);
+
+  return Response.json(newMessage);
+}
